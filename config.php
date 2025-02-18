05c668e3a8123a53f83c92ee42dfdab06f07cf32
korniszon99 <kornel.musial@gmail.com>
2025-02-18 15:46:09 +0100

    config pod app service bez ssl

--- config.php
+++ config.php
@@ -5,26 +5,22 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Pobierz dane z Azure App Service – zmienne środowiskowe
+// Pobierz dane z Azure App Service
 $db_host = getenv('DB_HOST');
 $db_user = getenv('DB_USER');
 $db_password = getenv('DB_PASSWORD');
 $db_name = getenv('DB_NAME');
 
-// Inicjalizacja połączenia
-$con = mysqli_init();
+try {
+    $con = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 
-// Ścieżka do certyfikatu SSL – Azure zaleca użycie DigiCertGlobalRootG2.crt.pem
-mysqli_ssl_set($con, NULL, NULL, '/site/wwwroot/DigiCertGlobalRootG2.crt.pem', NULL, NULL);
+    if (!$con) {
+        throw new Exception("Błąd połączenia MySQL: " . mysqli_connect_error());
+    }
 
+    echo "Połączono z bazą danych!";
 
-// Połączenie
-mysqli_real_connect($con, $db_host, $db_user, $db_password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
-
-// Sprawdzenie połączenia
-if (mysqli_connect_errno($con)) {
-    die("Błąd połączenia z bazą danych: " . mysqli_connect_error());
+} catch (Exception $e) {
+    die("Wystąpił błąd: " . $e->getMessage());
 }
-
-echo "Połączono z bazą danych!";
 ?>
