da7722621e24ee29f835fd1d4eeb8a3794afdc92
korniszon99 <kornel.musial@gmail.com>
2025-02-18 15:26:31 +0100

    config pod app service

--- config.php
+++ config.php
@@ -5,11 +5,26 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$con = mysqli_init();
-mysqli_ssl_set($con, NULL, NULL, '/Users/korniszon99/PhpstormProjects/SzkoleniePhotoTaskGame/DigiCertGlobalRootG2.crt.pem', NULL, NULL);
-mysqli_real_connect($con, 'szkoleniesql.mysql.database.azure.com', 'adminPW', 'piktu6-buFduk-dizcyg', 'fut_gdansk_app', 3306, MYSQLI_CLIENT_SSL);
+// Pobierz dane z Azure App Service – zmienne środowiskowe
+$db_host = getenv('DB_HOST');
+$db_user = getenv('DB_USER');
+$db_password = getenv('DB_PASSWORD');
+$db_name = getenv('DB_NAME');
 
+// Inicjalizacja połączenia
+$con = mysqli_init();
+
+// Ścieżka do certyfikatu SSL – Azure zaleca użycie DigiCertGlobalRootG2.crt.pem
+mysqli_ssl_set($con, NULL, NULL, '/site/wwwroot/DigiCertGlobalRootG2.crt.pem', NULL, NULL);
+
+
+// Połączenie
+mysqli_real_connect($con, $db_host, $db_user, $db_password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+// Sprawdzenie połączenia
 if (mysqli_connect_errno($con)) {
     die("Błąd połączenia z bazą danych: " . mysqli_connect_error());
 }
+
+echo "Połączono z bazą danych!";
 ?>
